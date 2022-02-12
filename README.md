e85dc6843e04f575c7b9ed4ec8afff9452689fda
Hiroaki Yutani <yutani.ini@gmail.com>
2022-02-12 18:19:08 +0900

    Finish

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # extract-R-API-items-from-header-file
 
+## 1-a. Extracted objects by clang (and some regex-fu)
+
 ``` pwsh
-# make sure llvm-config is on path
-$env:PATH = "C:\msys64\mingw64\bin;${env:PATH}"
+# Make sure llvm-config is on path.
+# 
+# e.g. $env:PATH = "C:\msys64\mingw64\bin;${env:PATH}"
 
 cargo +stable-gnu run > dump.txt
+```
+
+## 2. "Non-API"
+
+Should these items be hided? But, we actually need some of them on embedded usages.
+
+``` pwsh
+Rscript -e "writeLines(tools:::nonAPI)" > nonAPI.txt
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use clang::EntityKind::*;
 use clang::*;
+use regex::Regex;
+use std::collections::HashSet;
+use std::io::{self, BufRead};
 
-fn main() {
+// This extract the items from include files in twos steps.
+// First, extract it using clang-rs. But, as this flatten the
+// #define macro, we need to extract macro constants (e.g. `#define FOO 1`)
+// and macro functions (e.g. `#define FOO(x) (x + 1)`) by ourselves.
+// Bindgen handles these things nicely, but I cannot understand how to do it...
+
+fn main() -> std::io::Result<()> {
     // Acquire an instance of `Clang`
     let clang = Clang::new().unwrap();
 
@@ -18,7 +27,8 @@ fn main() {
         .parse()
         .unwrap();
 
-    // Get the structs in this translation unit
+    let mut include_files = HashSet::new();
+
     let e = tu
         .get_entity()
         .get_children()
@@ -27,7 +37,13 @@ fn main() {
             Some(l) => match l.get_file_location().file {
                 Some(f) => {
                     let p = f.get_path();
-                    p.to_string_lossy().contains(r_home)
+
+                    if p.to_string_lossy().contains(r_home) {
+                        include_files.insert(p);
+                        true
+                    } else {
+                        false
+                    }
                 }
                 None => false,
             },
@@ -45,4 +61,29 @@ fn main() {
             ek => panic!("Unknown kind: {ek:?}"),
         }
     }
+
+    // case 1) numeric literals
+    //
+    //     #define FOO 1
+    //
+    // case 2) string literals
+    //
+    //     #define FOO "foo"
+    //
+    // case 3) inline function
+    //
+    //     #define FOO(x) (x + 1)
+    //
+    let re = Regex::new(r#"^\s*#\s*define\s+([^\s\(]+)(\s*\(|\s+[0-9"])"#).unwrap();
+
+    for include_file in include_files {
+        let file = std::fs::File::open(include_file)?;
+        for line in io::BufReader::new(file).lines() {
+            if let Some(cap) = re.captures(line?.as_str()) {
+                println!("{}", &cap[1]);
+            }
+        }
+    }
+
+    Ok(())
 }
